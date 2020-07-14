ed7b9c7b1b86a88773b7c97d1ba910f997eddc34
dvash999 <myselfandme@gmail.com>
2020-07-15 01:30:17 +0300

    m

--- server/db/index.ts
+++ server/db/index.ts
@@ -4,12 +4,19 @@ export {};
 const mysql = require('mysql');
 
 const db = mysql.createPool({
-  connectionLimit: process.env.CONNECTION_LIMIT,
-  password: process.env.PASSWORD,
-  user: 'root',
-  database: process.env.DB,
-  host: process.env.HOST,
-  port: process.env.PORT,
+  connectionLimit: 10,
+  password: 'JVUy&nr0en%',
+  user: 'admin',
+  database: 'pit',
+  host: '3306',
+  port: 'database-aws.ca6u1sz8llmi.eu-west-1.rds.amazonaws.com',
+
+  // connectionLimit: process.env.CONNECTION_LIMIT,
+  // password: process.env.PASSWORD,
+  // user: 'root',
+  // database: process.env.DB,
+  // host: process.env.HOST,
+  // port: process.env.PORT,
 });
 
 module.exports = db;
