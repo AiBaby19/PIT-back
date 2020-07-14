e23a76fcafa3053b3fa4aef18278dbb0ff61973d
dvash999 <myselfandme@gmail.com>
2020-07-15 01:53:42 +0300

    m

--- server/db/index.ts
+++ server/db/index.ts
@@ -13,7 +13,7 @@ const db = mysql.createPool({
 
   connectionLimit: process.env.CONNECTION_LIMIT,
   password: process.env.PASSWORD,
-  user: 'root',
+  user: process.env.USER,
   database: process.env.DB,
   host: process.env.HOST,
   port: process.env.PORT,
